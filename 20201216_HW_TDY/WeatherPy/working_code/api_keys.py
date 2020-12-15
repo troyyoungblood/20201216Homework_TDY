1bca2f4310d7181dac9447bae3a53b6253f7435c
troyyoungblood <72400175+troyyoungblood@users.noreply.github.com>
2020-12-15 17:09:44 -0600

    removed keys from file

--- 20201216_HW_TDY/WeatherPy/working_code/api_keys.py
+++ 20201216_HW_TDY/WeatherPy/working_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "bfabad3eec079b77cd6e28110d3ad22f"
+weather_api_key = "HARRY's KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyBKZyrmnIxatibyVFhiPyCHD87TQW0ulAA"
+g_key = "HARRY's KEY HERE"
